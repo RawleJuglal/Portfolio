114c2a941a193a7a6ea181b09975738514322231
Rawle Juglal <rawledjuglal@gmail.com>
2022-11-15 20:24:36 -0600

    added feature
    Images with a hover zoom

---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Works.css'
+
+export default function Works(){
+    return(
+        <div className='--works--works-container'>
+            <div className='--works-image-overlay --works-image-colorize'>
+                <img className='--works-img --works-item1' src="/images/proof.jpg"></img>
+            </div>
+        </div>
+    )
+}
