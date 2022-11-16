eaa1a318779bfe380a5b9ec7c74ffdbcde1ca71e
Rawle Juglal <rawledjuglal@gmail.com>
2022-11-15 22:07:19 -0600

    added feature
    data for list of works

---
+++ src/worksData.js
@@ -0,0 +1,60 @@
+import {nanoid} from 'nanoid'
+
+export default [
+    {
+        id:nanoid(),
+        title:'Quizzical',
+        imgUrl:'/Portfolio/images/proof.jpg',
+        siteLink:'#',
+        githubLink:'https://github.com/RawleJuglal/Quizzical'
+    },
+    {
+        id:nanoid(),
+        title:'Tenzies',
+        imgUrl:'/Portfolio/images/proof.jpg',
+        siteLink:'#',
+        githubLink:'https://github.com/RawleJuglal/Tenzies'
+    },
+    {
+        id:nanoid(),
+        title:'ReactMDE',
+        imgUrl:'/Portfolio/images/proof.jpg',
+        siteLink:'#',
+        githubLink:'https://github.com/RawleJuglal/ReactMDE'
+    },
+    {
+        id:nanoid(),
+        title:'Meme Generator',
+        imgUrl:'/Portfolio/images/proof.jpg',
+        siteLink:'#',
+        githubLink:'https://github.com/RawleJuglal/MemeGenerator'
+    },
+    {
+        id:nanoid(),
+        title:'Travel Journal',
+        imgUrl:'/Portfolio/images/proof.jpg',
+        siteLink:'#',
+        githubLink:'https://github.com/RawleJuglal/TravelJournal'
+    },
+    {
+        id:nanoid(),
+        title:'AirBnb Clone',
+        imgUrl:'/Portfolio/images/proof.jpg',
+        siteLink:'#',
+        githubLink:'https://github.com/RawleJuglal/AirBnbClone'
+    },
+    {
+        id:nanoid(),
+        title:'Digital Business Card',
+        imgUrl:'/Portfolio/images/proof.jpg',
+        siteLink:'#',
+        githubLink:'https://github.com/RawleJuglal/DigitalBusinessCard'
+    },
+    {
+        id:nanoid(),
+        title:'React Static Website',
+        imgUrl:'/Portfolio/images/proof.jpg',
+        siteLink:'#',
+        githubLink:'https://github.com/RawleJuglal/ReactStatic'
+    },
+]
